283a248f6e5ab6800667809a3c21f1b977fac16e
Thiago <thiagobussola@hotmail.com>
2021-08-12 09:05:23 -0300

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  extends: 'standard-with-typescript',
+  parserOptions: {
+    project: './tsconfig.json'
+  },
+  rules: {
+    'no-console': 2,
+    '@typescript-eslint/strict-boolean-expressions': 0,
+    '@typescript-eslint/explicit-function-return-type': 0,
+    '@typescript-eslint/no-throw-literal': 0,
+    '@typescript-eslint/return-await': 0,
+    '@typescript-eslint/restrict-template-expressions': 0
+  }
+}
